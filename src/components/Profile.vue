883863485afcc2393fce8d1b765c320320d96f47
EC2 Default User <ec2-user@ip-172-31-15-200.us-east-2.compute.internal>
2021-03-10 15:40:07 +0000

    I dunno

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -45,7 +45,7 @@ export default {
       };
 
       
-      console.log("STORE STATE: ", this.$store.state.user)
+      console.log("STORE STATE: ", this.$store.state.user.e)
       console.log("USERNAME1: ", this.username);
       console.log("USERNAME2: ", this.myState);
 
