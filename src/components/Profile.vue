1d37d5b7932a46367524135907de092140cbd396
EC2 Default User <ec2-user@ip-172-31-15-200.us-east-2.compute.internal>
2021-03-10 14:39:59 +0000

    Fixing crash

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -29,11 +29,11 @@ export default {
   methods: {
     async getPerson() {
       const { variables } = {
-        id: this.username(),
+        id: this.username,
       };
       
       console.log("STORE STATE: ", this.$store.state)
-      console.log("USERNAME: ", this.username());
+      console.log("USERNAME: ", this.username);
 
       const user = await API.graphql({
         query: getPerson,
